a30a0bdb9cfb115c0e7f28193d3165b7135ca396
Nishank Phulera <79889057+nishankphulera@users.noreply.github.com>
2023-06-12 21:22:14 +0530

    Add files via upload

---
+++ types.tsx
@@ -0,0 +1,60 @@
+/**
+ * Learn more about using TypeScript with React Navigation:
+ * https://reactnavigation.org/docs/typescript/
+ */
+
+import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
+import { CompositeScreenProps, NavigatorScreenParams } from '@react-navigation/native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
+
+export type RootStackParamList = {
+  Root: NavigatorScreenParams<RootTabParamList> | undefined;
+  Modal: undefined;
+  NotFound: undefined;
+  SongsList:{name:string,
+        by:string,
+        likes:string,
+        purchased:string,
+        image:string,
+        price:string},
+  Player:{
+    name:string,
+    liked:boolean,
+    image:string,
+    duration:number,
+    uri:string
+  },
+  Landing:undefined,
+  Login:undefined,
+  Signup:undefined
+        };
+
+export type RootStackScreenProps<Screen extends keyof RootStackParamList> = NativeStackScreenProps<
+  RootStackParamList,
+  Screen
+>;
+
+export type RootTabParamList = {
+  Home: undefined;
+  Search: undefined;
+  Playlist: undefined;
+  Cart: undefined;
+  
+  
+};
+// Songs:{name:string,
+// by:string,
+// likes:string,
+// purchased:string,
+// image:string}
+
+export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
+  BottomTabScreenProps<RootTabParamList, Screen>,
+  NativeStackScreenProps<RootStackParamList>
+>;
